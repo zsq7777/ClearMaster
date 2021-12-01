721b2c2f084d37465f5148be43e35c53c4c49571
zhaosiqi <zsqandzyr@qq.com>
2021-12-01 17:50:20 +0800

    firstSubmit

---
+++ app/src/main/java/com/able/clearmaster/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.able.clearmaster.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.able.clearmaster.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
